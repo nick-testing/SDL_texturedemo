0c26af5c5bf96c5b87d31c3dc1cefcea379a3e39
Nick <80259776+nick-testing@users.noreply.github.com>
2024-08-16 20:04:28 +0300

    Added SDL and SDL_Image initialization

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,3 +4,43 @@
 #include <iostream>
 
 Game::Game(): window(NULL), renderer(NULL) {};
+
+/**
+ * Initialize SDL and SDL_image libraries.
+ * Init() also creates an SDL window and a hardware accelerated SDL renderer for that window.
+ * 
+ * \return true on succes, false on failure
+ */
+bool Game::Init() {
+    bool success = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL initialization failed, SDL error: " << SDL_GetError() << std::endl;
+        success = false;
+    }
+    else {
+        window = SDL_CreateWindow("SDL Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                    SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (!window) {
+            std::cerr << "SDL window creation failed, SDL error: " << SDL_GetError() << std::endl;
+            success = false;
+        }
+        else {
+            // Create renderer for window
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+            if (!renderer) {
+                std::cerr << "SDL CreateRenderer failed, SDL error: " << SDL_GetError() << std::endl;
+                success = false;
+            }
+            else {
+                // Initialize SDL image PNG loading
+                if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+                    std::cerr << "SDL_Image init failed, SDL error: " << SDL_GetError() << std::endl;
+                    success = false;
+                }
+            }
+        }
+    }
+
+    return success;
+}
