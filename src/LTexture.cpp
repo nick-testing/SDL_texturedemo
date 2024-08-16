995b3ed973c8c23779a0c9baa449cbba69256604
Nick <80259776+nick-testing@users.noreply.github.com>
2024-08-16 15:55:59 +0300

    Added member function definitions

--- src/LTexture.cpp
+++ src/LTexture.cpp
@@ -7,3 +7,23 @@ LTexture::LTexture() : width(0), height(0), texture(nullptr) {}
 LTexture::~LTexture() {
     Free();
 }
+
+bool LTexture::LoadFromFile(const char* path) {
+
+}
+
+void LTexture::Render(int x, int y) {
+
+}
+
+int LTexture::GetWidth() {
+    return width;
+}
+
+int LTexture::GetHeight() {
+    return height;
+}
+
+void LTexture::Free() {
+    SDL_DestroyTexture(texture);
+}
