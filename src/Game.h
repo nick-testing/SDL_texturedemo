4528404c5b60a7d823598490be37c144d7d2089c
Nick <80259776+nick-testing@users.noreply.github.com>
2024-08-16 20:02:50 +0300

    Added Game driver class header file

---
+++ src/Game.h
@@ -0,0 +1,34 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "LTexture.h"
+
+typedef struct SDL_Window SDL_Window;
+typedef struct SDL_Renderer SDL_Renderer;
+typedef struct SDL_Texture SDL_Texture;
+
+class Game {
+public:
+    Game();
+    void Run();
+
+private:
+    static const int SCREEN_HEIGHT = 600;
+    static const int SCREEN_WIDTH = 800;
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    LTexture backgroundTexture;
+    LTexture foregroundTexture;
+    
+    bool Init();
+    bool LoadMedia();
+
+
+    void Close();
+
+    void RenderTexture();
+    void RenderViewport();
+};
+
+#endif
