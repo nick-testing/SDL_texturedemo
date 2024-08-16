8e828aac8a035ce036db5db717955569078019a6
Nick <80259776+nick-testing@users.noreply.github.com>
2024-08-16 19:34:37 +0300

    Added LoadFromFile() function

--- src/LTexture.cpp
+++ src/LTexture.cpp
@@ -13,7 +13,33 @@ LTexture::~LTexture() {
 }
 
 bool LTexture::LoadFromFile(SDL_Renderer* renderer, const char* path) {
+    // Free previously held texture
+    Free();
+    
+    SDL_Surface* loadedSurface = IMG_Load(path);
+    if (!loadedSurface) {
+        std::cerr << "Image loading failed, SDL error: " << SDL_GetError() << std::endl;
+    }
+    else {
+        // Set the color key(transparent pixel color) in the loaded surface
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, COLOR_CHANNEL_MIN, 
+                        COLOR_CHANNEL_MAX, COLOR_CHANNEL_MAX));
 
+        // Create a texture from the color keyed loaded surface
+        texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if (!texture) {
+            std::cerr << "Texture creation failed, SDL error: " << SDL_GetError() << std::endl;
+        }
+        else {
+            // Set image dimensions for the texture
+            width = loadedSurface->w;
+            height = loadedSurface->h;
+        }
+
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return texture;
 }
 
 void LTexture::Render(SDL_Renderer* renderer, int x, int y) {
