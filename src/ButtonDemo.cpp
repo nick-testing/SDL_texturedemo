1d9997e968853690fa85bb8a44df071e166d1e8b
Nick <80259776+nick-testing@users.noreply.github.com>
2024-08-21 21:30:14 +0300

    Added button demo function

---
+++ src/ButtonDemo.cpp
@@ -0,0 +1,56 @@
+#include "ButtonDemo.h"
+#include <SDL2/SDL.h>
+
+ButtonDemo::ButtonDemo() : SDLProgram() {}
+
+bool ButtonDemo::LoadMedia() {
+    bool loadSuccessful = true;
+
+    for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i) {
+        loadSuccessful = buttons[i].LoadMedia(renderer, "assets/button.png");
+    }
+    buttons[1].SetPosition(300,0);
+    buttons[2].SetPosition(0, 200);
+    buttons[3].SetPosition(300, 200);
+
+    return loadSuccessful;
+}
+
+void ButtonDemo::RenderLoop() {
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while(SDL_PollEvent(&e)) {
+            if (SDL_QUIT == e.type) {
+                quit = true;
+            }
+            for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i) {
+                buttons[i].HandleEvent(&e);
+            }
+        }
+
+        ClearScreen();
+        for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i) {
+            buttons[i].Render(renderer);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+}
+
+void ButtonDemo::Close() {
+    for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i) {
+        buttons[i].Free();
+    }
+    SDLProgram::Close();
+}
+
+void ButtonDemo::Run() {
+    if(Init()) {
+        LoadMedia();
+        RenderLoop();
+    }
+    Close();
+
+}
---
+++ src/ButtonDemo.h
@@ -0,0 +1,19 @@
+#ifndef BUTTON_DEMO_H
+#define BUTTON_DEMO_H
+
+#include "SDLProgram.h"
+#include "Button.h"
+
+class ButtonDemo : SDLProgram {
+public:
+    ButtonDemo();
+    void Run() override;
+private:
+    Button buttons[BUTTON_SPRITE_TOTAL];
+
+    bool LoadMedia() override;
+    void RenderLoop() override;
+    void Close() override;
+};
+
+#endif
